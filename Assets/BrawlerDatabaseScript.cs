7832fa1de56b8e69c2b20d4378faece6fe3f75d3
Brendan King <brendan@brendanking.com>
2020-08-15 17:13:08 -0400

    Hopefully fixed the lag issue

--- Assets/BrawlerDatabaseScript.cs
+++ Assets/BrawlerDatabaseScript.cs
@@ -22,7 +22,6 @@ public class BrawlerDatabaseScript : MonoBehaviour {
     public GameObject[] errors;
     public GameObject[] buttonTexts;
 
-    private string[] glitches = new string[25];
     private string[] names = new string[] { "Ace", "Airzel", "Alice", "Anubias", "Barodius", "Baron", "Dan", "Fabia", "Gill", "Prince Hydron", "Jake", "Julie", "Kazarina", "Marucho", "Masquerade", "Mira", "Mylene", "Nurzak", "Ren", "Runo", "Sellon", "Shun", "Spectra", "Stoica", "Volt", "King Zenoheld" };
     private string[] homes = new string[] { "Earth", "Gundalia", "N/A", "Neathia", "Vestal" };
     private string[] attributes = new string[] { "Aquos", "Darkus", "Haos", "Pyrus", "Subterra", "Ventus" };
@@ -54,16 +53,6 @@ public class BrawlerDatabaseScript : MonoBehaviour {
     }
 
     void Start () {
-        for (int i = 0; i < 25; i++)
-        {
-            char[] valids = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-            string make = "";
-            for (int j = 0; j < 8; j++)
-            {
-                make += valids[UnityEngine.Random.Range(0, valids.Length)];
-            }
-            glitches[i] = make;
-        }
         chosenChar = UnityEngine.Random.Range(0, characterImgs.Length);
         characterDisp.sprite = characterImgs[chosenChar];
         Debug.LogFormat("[Brawler Database #{0}] The brawler displayed is {1}", moduleId, names[chosenChar]);
@@ -328,21 +317,29 @@ public class BrawlerDatabaseScript : MonoBehaviour {
 
     private IEnumerator glitchText()
     {
-        for (int i = 0; i < 25; i++)
+        char[] valids = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        string make = "";
+        while (true)
         {
-            smallDisp.text = glitches[i];
-            yield return new WaitForSeconds(0.06f);
+            make = "";
+            for (int j = 0; j < 8; j++)
+            {
+                make += valids[UnityEngine.Random.Range(0, valids.Length)];
+            }
+            smallDisp.text = make;
+            yield return null;
         }
-        glitch = StartCoroutine(glitchText());
     }
 
     private IEnumerator errorFlash(int num)
     {
-        errors[num].SetActive(true);
-        yield return new WaitForSeconds(0.1f);
-        errors[num].SetActive(false);
-        yield return new WaitForSeconds(0.1f);
-        errorCorouts[num] = StartCoroutine(errorFlash(num));
+        while (true)
+        {
+            errors[num].SetActive(true);
+            yield return new WaitForSeconds(0.1f);
+            errors[num].SetActive(false);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     //twitch plays
